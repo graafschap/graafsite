292dc7b589b439a23cc8c86afd945ac353b3ca3c
Jeffrey <Jeffrey@defjef.nl>
2022-07-21 23:25:46 +0200

    Added new front-end with tiles nad contact

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BannerComponent } from './banner/banner.component';
+import { ContentTileComponent } from './content-tile/content-tile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BannerComponent
+    BannerComponent,
+    ContentTileComponent
   ],
   imports: [
     BrowserModule
