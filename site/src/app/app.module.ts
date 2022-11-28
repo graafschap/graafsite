b96a17215f7bffa4147f431a99e9f2bc8a4a2952
Jeffrey Cornelissen <jeffrey.cornelissen@fontys.nl>
2022-11-28 21:09:13 +0100

    Commit before upgrade to angular cli/core 14

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BannerComponent } from './banner/banner.component';
+import { WelkomComponent } from './welkom/welkom.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BannerComponent
+    BannerComponent,
+    WelkomComponent
   ],
   imports: [
     BrowserModule
