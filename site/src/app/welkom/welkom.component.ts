8ac5a1ba1e056d0d88f4a939621c480475542162
Jeffrey Cornelissen <jeffrey.cornelissen@fontys.nl>
2022-11-28 21:10:30 +0100

    Commit before upgrade to angular cli/core 14

---
+++ site/src/app/welkom/welkom.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'graafschap-welkom',
+  templateUrl: './welkom.component.html',
+  styleUrls: ['./welkom.component.css']
+})
+export class WelkomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
