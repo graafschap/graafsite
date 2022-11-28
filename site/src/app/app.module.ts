ffdc9efa7d7207514059963e3ba938d659a03bbc
Jeffrey Cornelissen <jeffrey.cornelissen@fontys.nl>
2022-11-28 22:02:20 +0100

    Changed the main page with the right information

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BannerComponent } from './banner/banner.component';
 import { WelkomComponent } from './welkom/welkom.component';
+import { GraafschapTileComponent } from './component/graafschap-tile/graafschap-tile.component';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerComponent,
-    WelkomComponent
+    WelkomComponent,
+    GraafschapTileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
