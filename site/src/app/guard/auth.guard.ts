7d1ca33fd8791178e2babc2f5216e05cf6ed8c88
Jeffrey Cornelissen <jeffrey.cornelissen@fontys.nl>
2023-04-24 22:24:28 +0200

    Commit before doing a pull from github

--- site/src/app/guard/auth.guard.ts
+++ site/src/app/guard/auth.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router, CanActivate } from '@angular/router';
 import { KeycloakAuthGuard, KeycloakService } from 'keycloak-angular';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard extends KeycloakAuthGuard {
+export class AuthGuard extends KeycloakAuthGuard implements CanActivate{
   
   constructor(
     protected override readonly router: Router,
@@ -26,4 +26,14 @@ export class AuthGuard extends KeycloakAuthGuard {
 
     return this.authenticated;
   }
+
+  public override async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean | UrlTree> {
+    if(await this.keycloak.isLoggedIn()){
+        return true;
+    }
+    else {
+        console.log("Nope can not activate")
+        return false;
+    }
+}
 }
--- site/src/app/init/keycloak-init.factory.ts
+++ site/src/app/init/keycloak-init.factory.ts
@@ -6,9 +6,19 @@ export function initializeKeycloak(
     return () =>
       keycloak.init({
         config: {
-          url: 'http://sso.defjef.nl' + '/auth',
-          realm: 'test',
-          clientId: 'frontend',
-        }
+          url: 'http://sso.defjef.nl',
+          realm: 'graafschap',
+          clientId: 'website',
+        },
+        initOptions: {
+          onLoad: 'check-sso',
+          silentCheckSsoRedirectUri:
+            // 'http://mko-gui-test.defjef.nl:8080/assets/silent-check-sso.html'
+            window.location.origin + '/assets/silent-check-sso.html'
+        },
+        bearerExcludedUrls: ['/assets', '/clients/public'],
+        shouldUpdateToken: (request) => {
+          return !request.headers.get('token-update') === false;
+        },            
       });
 }
